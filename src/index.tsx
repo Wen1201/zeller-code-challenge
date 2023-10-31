1e889f68d3734f71a45c5635d640d28d49a6a7d1
wgongSummerTech <wgong@summertech.com.au>
2023-10-31 23:29:39 +1100

    Add aws amplify

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Amplify } from 'aws-amplify';
+import config from './config/aws-export';
+
+Amplify.configure(config);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
